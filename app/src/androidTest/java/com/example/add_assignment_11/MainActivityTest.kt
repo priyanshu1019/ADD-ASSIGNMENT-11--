2ffb51eeb1477a7452a8af339aed686e572708e5
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-20 13:43:32 +0530

    recorded ui test using espresao

---
+++ app/src/androidTest/java/com/example/add_assignment_11/MainActivityTest.kt
@@ -0,0 +1,302 @@
+package com.example.add_assignment_11
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.etUi), withText("Name"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("Nam"))
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Nam"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Nam"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(click())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Nam"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText(""))
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.etUi),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(closeSoftKeyboard())
+
+        val appCompatEditText6 = onView(
+            allOf(
+                withId(R.id.etUi),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText6.perform(click())
+
+        val appCompatEditText7 = onView(
+            allOf(
+                withId(R.id.etUi),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText7.perform(replaceText("rianshu "), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btnUi), withText("Button"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val appCompatEditText8 = onView(
+            allOf(
+                withId(R.id.etUi), withText("rianshu "),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText8.perform(replaceText("rianshu f"))
+
+        val appCompatEditText9 = onView(
+            allOf(
+                withId(R.id.etUi), withText("rianshu f"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText9.perform(closeSoftKeyboard())
+
+        val appCompatEditText10 = onView(
+            allOf(
+                withId(R.id.etUi), withText("rianshu f"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText10.perform(click())
+
+        val appCompatEditText11 = onView(
+            allOf(
+                withId(R.id.etUi), withText("rianshu f"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText11.perform(replaceText("rianshu fru"))
+
+        val appCompatEditText12 = onView(
+            allOf(
+                withId(R.id.etUi), withText("rianshu fru"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText12.perform(closeSoftKeyboard())
+
+        val viewGroup = onView(
+            allOf(
+                withId(R.id.main),
+                withParent(
+                    allOf(
+                        withId(android.R.id.content),
+                        withParent(withId(com.google.android.material.R.id.action_bar_root))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
