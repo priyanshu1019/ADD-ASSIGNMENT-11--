3d6266f3970e699e3b257a2181de544d8f361445
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-20 13:34:47 +0530

    recorded ui test

---
+++ app/src/main/java/com/example/add_assignment_11/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.add_assignment_11
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun getShowText(view: View){
+        var uiEt : EditText = findViewById(R.id.etUi)
+        var textTyped = uiEt.text.toString()
+
+        var uiTv : TextView = findViewById(R.id.tvUi)
+        uiTv.setText( textTyped)
+    }
+}
